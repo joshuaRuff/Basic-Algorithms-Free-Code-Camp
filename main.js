6916e61c98357e24f1154f47c1ba2a7703d133b4
Crypto Ruff <Cryptoruff@Wayne-Ruffs-MacBook-Pro.local>
2018-05-15 14:08:37 -0500

    added the main JS algorithm

---
+++ main.js
@@ -0,0 +1,6 @@
+function reverseString(str) {
+  //simple array methods applied to reverse a string
+  return str.split("").reverse().join("");
+}
+ //calling the "reverseString" function with argument to be reversed
+reverseString("hello");
